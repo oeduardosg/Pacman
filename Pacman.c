aa8d571ff3ecb1b06605ce8653e62977b3d01781
oeduardosg <eduardosg.my@gmail.com>
2023-07-10 11:57:40 -0300

    Os fantasmas se divertem - agora falta fazer as estatísticas

--- Pacman.c
+++ Pacman.c
@@ -16,6 +16,12 @@
 #define MAPA_MAX_C 100
 #define MAX_FANTASMAS 4
 
+//Jogadas
+#define CIMA 'w'
+#define ESQUERDA 'a'
+#define BAIXO 's'
+#define DIREITA 'd'
+
 //TipoCoordenada
 typedef struct {
     int x;
@@ -24,6 +30,7 @@ typedef struct {
 
 //Tipo Pacman
 typedef struct {
+    int vida;
     tCoordenada coordenada;
     int pontuacao;
 } tPacman;
@@ -32,9 +39,12 @@ typedef struct {
 typedef struct {
     char letra;
     tCoordenada coordenada;
+    tCoordenada coordenadaAnterior;
     int dx;
     int dy;
 } tFantasma;
+
+//Tipo Movimento
 
 //Tipo Jogo
 typedef struct {
@@ -45,6 +55,14 @@ typedef struct {
     tFantasma fantasmas[MAX_FANTASMAS];
     char mapa[MAPA_MAX_L][MAPA_MAX_C];
 } tJogo;
+
+typedef struct {
+    char direcao;
+    int comeu;
+    int bateu;
+    int morreu;
+    tCoordenada coordenada;
+} tMovimento;
 
 
 //!!! FIM DOS TIPOS !!!
@@ -178,13 +196,20 @@ void InicializaFantasmas(tFantasma * fantasma) {
     }
 }
 
+tPacman InicializaPacman(int linha, int coluna, char mapaGeral[linha][coluna]) {
+    tPacman pacman;
+    pacman.coordenada = EncontraCoordenada(linha, coluna, mapaGeral, PACMAN);
+    pacman.vida = 1;
+    pacman.pontuacao = 0;
+return pacman;
+}
+
 //Adiciona as informações que faltam no jogo (fantasmas e pacman)
 tJogo LeJogoCompleto(int linha, int coluna, char mapaGeral[linha][coluna], tJogo jogo) {
     int i, j;
 
     tPacman pacman;
-    pacman.coordenada = EncontraCoordenada(linha, coluna, mapaGeral, PACMAN);
-    pacman.pontuacao = 0;
+    pacman = InicializaPacman(linha, coluna, mapaGeral);
     jogo.pacman = pacman;
 
     tFantasma fantasmas[MAX_FANTASMAS];
@@ -249,18 +274,21 @@ tPacman AtualizaCoordenadaPacman(int x, int y, tPacman pacman) {
 return pacman;
 }
 
-int AchaComidas(int linha, int coluna, char mapa[linha][coluna]) {
+int AchaComidas(int linha, int coluna, tJogo jogo) {
     int i, j, comidas = 0;
     for(i = 0; i < linha; i++) {
         for(j = 0; j < coluna; j++) {
-            if(mapa[i][j] == COMIDA) comidas++;
+            if(jogo.mapa[i][j] == COMIDA) comidas++;
         }
     }
 return comidas;
 }
 
+//Troca as coordenadas de um fantasma qualquer durante o turno
 tFantasma AtualizaCoordenadaFantasma(tFantasma fantasma, tJogo jogo) {
     int i, j;
+    fantasma.coordenadaAnterior.x = fantasma.coordenada.x;
+    fantasma.coordenadaAnterior.y = fantasma.coordenada.y;
     if(fantasma.dx != 0) {
         if(jogo.mapa[fantasma.coordenada.x+fantasma.dx][fantasma.coordenada.y] == PAREDE) {
             fantasma.dx *= -1;
@@ -282,28 +310,49 @@ tFantasma AtualizaCoordenadaFantasma(tFantasma fantasma, tJogo jogo) {
 return fantasma;
 }
 
-int TemFantasmaAqui(tFantasma * fantasmas, int x, int y) {
+//Verifica a existência de um fantasma na coordenada
+int TemFantasmaAqui(tFantasma * fantasmas, int x, int y, char jogada) {
     int i;
     for(i = 0; i < MAX_FANTASMAS; i++) {
         if(fantasmas[i].coordenada.x == x && fantasmas[i].coordenada.y == y) return 1;
+        if(fantasmas[i].dx == 1 && fantasmas[i].coordenadaAnterior.x == x && fantasmas[i].coordenadaAnterior.y == y && jogada == CIMA) return 1;
+        if(fantasmas[i].dx == -1 && fantasmas[i].coordenadaAnterior.x == x && fantasmas[i].coordenadaAnterior.y == y && jogada == BAIXO) return 1;
+        if(fantasmas[i].dy == 1 && fantasmas[i].coordenadaAnterior.x == x && fantasmas[i].coordenadaAnterior.y == y && jogada == ESQUERDA) return 1;
+        if(fantasmas[i].dy == -1 && fantasmas[i].coordenadaAnterior.x == x && fantasmas[i].coordenadaAnterior.y == y && jogada == DIREITA) return 1;
     }
 return 0;
 }
 
+tPacman AtualizaVidaPacman(tPacman pacman) {
+    pacman.vida--;
+return pacman;
+}
+
+int GetLifePacman(tPacman pacman) {
+return pacman.vida;
+}
+
+tMovimento PreencheMovimento(char direcao, int comeu, int bateu, tPacman pacman) {
+    tMovimento movimento;
+    movimento.direcao = direcao;
+    movimento.comeu = comeu;
+    movimento.bateu = bateu;
+    movimento.morreu = pacman.vida;
+    movimento.coordenada = pacman.coordenada;
+return movimento;
+}
+
 //Função que deve realizar as jogadas até que todas as comidas sejam consumidas ou o jogador perca
 void RealizaJogadas(tJogo jogo) {
-    int jogadas = 0, pontuacao = 0, comidas = 0, vida = 1;
+    int jogadas = 0, pontuacao = 0, comidas = 0, vida = 1, i;
     char jogada, lixo;
 
-    int i, j;
-    for(i = 0; i < jogo.linhas; i++) {
-        for(j = 0; j < jogo.colunas; j++) {
-            if(jogo.mapa[i][j] == COMIDA) comidas++;
-        }
-    }
+    tMovimento movimentos[jogo.limiteDeJogadas];
 
     tCoordenada xyPacman;
     int xPacman, yPacman;
+
+    comidas = AchaComidas(jogo.linhas, jogo.colunas, jogo);
 
     while(comidas != pontuacao && jogadas <= jogo.limiteDeJogadas) {
         scanf("%c%*c", &jogada);
@@ -319,66 +368,134 @@ void RealizaJogadas(tJogo jogo) {
         yPacman = GetY(xyPacman);
 
         //FALTA MEXER EM TODAS AS MOVIMENTAÇÕES E ADICIONAR QUANDO MORRE PRO FANTASMA! 
-        //A MOVIMENTAÇÃO "d" PRECISA SER MODIFICADA, NÃO ESTÁ CORRETA
         //MODIFICAR OS PRINTF NO FINAL E TAMBÉM REVER A CONTAGEM DE JOGADAS
 
         switch (jogada) {
-            case 'w':
+            case CIMA:
                 if(jogo.mapa[xPacman-1][yPacman] != PAREDE) {
-                    if(jogo.mapa[xPacman-1][yPacman] == COMIDA) {
-                        pontuacao++;
-                        jogo.mapa[xPacman-1][yPacman] = VAZIO;
+                    if(TemFantasmaAqui(jogo.fantasmas, xPacman-1, yPacman, jogada)) {
+                        jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                        vida = GetLifePacman(jogo.pacman);
+                        if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(CIMA, 0, 0, jogo.pacman);
                     }
-                    jogo.pacman = AtualizaCoordenadaPacman(xPacman-1, yPacman, jogo.pacman);
+                    else {
+                        jogo.pacman = AtualizaCoordenadaPacman(xPacman-1, yPacman, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(CIMA, 0, 0, jogo.pacman);
+                        if(jogo.mapa[xPacman-1][yPacman] == COMIDA) {
+                            pontuacao++;
+                            jogo.mapa[xPacman-1][yPacman] = VAZIO;
+                            movimentos[jogadas] = PreencheMovimento(CIMA, 1, 0, jogo.pacman);
+                        }
+                    }
+                }
+                else if(TemFantasmaAqui(jogo.fantasmas, xPacman, yPacman, jogada)) {
+                    jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                    vida = GetLifePacman(jogo.pacman);
+                    if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                    movimentos[jogadas] = PreencheMovimento(CIMA, 0, 0, jogo.pacman);
+                }
+                else {
+                    movimentos[jogadas] = PreencheMovimento(CIMA, 0, 1, jogo.pacman);
                 }
                 break;
             
-            case 'a':
+            case ESQUERDA:
                 if(jogo.mapa[xPacman][yPacman-1] != PAREDE) {
-                    if(jogo.mapa[xPacman][yPacman-1] == COMIDA) {
-                        pontuacao++;
-                        jogo.mapa[xPacman][yPacman-1] = VAZIO;
-                    }
-                    jogo.pacman = AtualizaCoordenadaPacman(xPacman, yPacman-1, jogo.pacman);
-                }
-                break;
-
-            case 's':
-                if(jogo.mapa[xPacman+1][yPacman] != PAREDE) {
-                    if(jogo.mapa[xPacman+1][yPacman] == COMIDA) {
-                        pontuacao++;
-                        jogo.mapa[xPacman+1][yPacman] = VAZIO;
-                    }
-                    jogo.pacman = AtualizaCoordenadaPacman(xPacman+1, yPacman, jogo.pacman);
-                }
-                break;
-
-            case 'd':
-                if(jogo.mapa[xPacman][yPacman+1] != PAREDE) {
-                    if(TemFantasmaAqui(jogo.fantasmas, xPacman, xPacman+1)) {
-                        vida = 0;
+                    if(TemFantasmaAqui(jogo.fantasmas, xPacman, yPacman-1, jogada)) {
+                        jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                        vida = GetLifePacman(jogo.pacman);
                         if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(ESQUERDA, 0, 0, jogo.pacman);
                     }
                     else {
+                        jogo.pacman = AtualizaCoordenadaPacman(xPacman, yPacman-1, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(ESQUERDA, 0, 0, jogo.pacman);
+                        if(jogo.mapa[xPacman][yPacman-1] == COMIDA) {
+                            pontuacao++;
+                            jogo.mapa[xPacman][yPacman-1] = VAZIO;
+                            movimentos[jogadas] = PreencheMovimento(ESQUERDA, 1, 0, jogo.pacman);
+                        }
+                    }
+                }
+                else if(TemFantasmaAqui(jogo.fantasmas, xPacman, yPacman, jogada)) {
+                    jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                    vida = GetLifePacman(jogo.pacman);
+                    if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                    movimentos[jogadas] = PreencheMovimento(ESQUERDA, 0, 0, jogo.pacman);
+                }
+                else {
+                    movimentos[jogadas] = PreencheMovimento(ESQUERDA, 0, 1, jogo.pacman);
+                }
+                break;
+
+            case BAIXO:
+                if(jogo.mapa[xPacman+1][yPacman] != PAREDE) {
+                    if(TemFantasmaAqui(jogo.fantasmas, xPacman+1, yPacman, jogada)) {
+                        jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                        vida = GetLifePacman(jogo.pacman);
+                        if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(BAIXO, 0, 0, jogo.pacman);
+                    }
+                    else {
+                        jogo.pacman = AtualizaCoordenadaPacman(xPacman+1, yPacman, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(BAIXO, 0, 0, jogo.pacman);
+                        if(jogo.mapa[xPacman+1][yPacman] == COMIDA) {
+                            pontuacao++;
+                            jogo.mapa[xPacman+1][yPacman] = VAZIO;
+                            movimentos[jogadas] = PreencheMovimento(BAIXO, 1, 0, jogo.pacman);
+                        }
+                    }
+                }
+                else if(TemFantasmaAqui(jogo.fantasmas, xPacman, yPacman, jogada)) {
+                    jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                    vida = GetLifePacman(jogo.pacman);
+                    if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                    movimentos[jogadas] = PreencheMovimento(BAIXO, 0, 0, jogo.pacman);
+                }
+                else {
+                    movimentos[jogadas] = PreencheMovimento(BAIXO, 0, 1, jogo.pacman);
+                }
+                break;
+
+            case DIREITA:
+                if(jogo.mapa[xPacman][yPacman+1] != PAREDE) {
+                    if(TemFantasmaAqui(jogo.fantasmas, xPacman, yPacman+1, jogada)) {
+                        jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                        vida = GetLifePacman(jogo.pacman);
+                        if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(DIREITA, 0, 0, jogo.pacman);
+                    }
+                    else {
+                        jogo.pacman = AtualizaCoordenadaPacman(xPacman, yPacman+1, jogo.pacman);
+                        movimentos[jogadas] = PreencheMovimento(DIREITA, 0, 0, jogo.pacman);
                         if(jogo.mapa[xPacman][yPacman+1] == COMIDA) {
                             pontuacao++;
                             jogo.mapa[xPacman][yPacman+1] = VAZIO;
+                            movimentos[jogadas] = PreencheMovimento(DIREITA, 1, 0, jogo.pacman);
                         }
-                        jogo.pacman = AtualizaCoordenadaPacman(xPacman, yPacman+1, jogo.pacman);
                     }
                 }
+                else if(TemFantasmaAqui(jogo.fantasmas, xPacman, yPacman, jogada)) {
+                    jogo.pacman = AtualizaVidaPacman(jogo.pacman);
+                    vida = GetLifePacman(jogo.pacman);
+                    if(!vida) jogo.pacman = AtualizaCoordenadaPacman(-1, -1, jogo.pacman);
+                    movimentos[jogadas] = PreencheMovimento(ESQUERDA, 0, 0, jogo.pacman);
+                }
+                else {
+                    movimentos[jogadas] = PreencheMovimento(DIREITA, 0, 1, jogo.pacman);
+                }
                 break;
+ 
         }
+        printf("Estado do jogo apos o movimento '%c':\n", jogada);
+        ImprimeJogo(jogo);
+        printf("Pontuacao: %d\n\n", pontuacao);
         if(comidas == pontuacao) {
             printf("Voce venceu!\nPontuacao final: %d\n", pontuacao);
         }
-        else if (jogadas == jogo.limiteDeJogadas || !vida) {
+        else if (jogadas == jogo.limiteDeJogadas - 1 || !vida) {
             printf("Game over!\nPontuacao final: %d\n", pontuacao);
-        }
-        else {
-            printf("Estado do jogo apos o movimento '%c':\n", jogada);
-            ImprimeJogo(jogo);
-            printf("Pontuacao: %d\n", pontuacao);
         }
         jogadas++;
     }
